111af85f73fd7e70d329f07177e0d6324f955efd
Vipul Somani <vipulsomani12@gmail.com>
2020-08-31 23:52:10 +0530

    updates in file

--- function.test.js
+++ function.test.js
@@ -1,4 +1,4 @@
-const functions = require('./functions');
+ const functions = require('./functions');
 
 test('summation of two number',() =>{
     expect(functions.add(2,5)).toBe(7);
@@ -7,6 +7,46 @@ test('summation of two number',() =>{
 test('summation of two number with not',() =>{
     expect(functions.add(2,5)).not.toBe(8);
 });
+// for to be null
 test('That must be null',() =>{
     expect(functions.isNULL()).toBeNull();
 });
+//for to be falsy value
+test('Expecting a false value',() =>{
+        expect(functions.checkValue(undefined)).toBeFalsy();
+});
+//toEqual 
+test('User must be Vipul Somani',() =>{
+    expect(functions.createUser()).toEqual({
+        firstname : 'Vipul',
+        lastname : 'Somani'
+    });
+});
+//less than and greater than
+
+test('Should be under 1500',() => {
+    const val1 = 900;
+    const val2 = 600;
+    expect(val1+val2).toBeLessThanOrEqual(1500);
+})
+
+//regex
+test('No m in Vipul',() => {
+    expect('Vipul').not.toMatch(/m/);
+})
+
+//Arrays
+
+test('vipul should be username', () => {
+    username = ['vipul','yash', 'yashi'];
+    expect(username).toContain('vipul');
+})
+
+//Workin with data
+test('User name Leanne Grahm', () => {
+    expect.assertions();
+    return functions.fetchUser().then( data => {
+        expect(data.name).toEqual('Leanne Graham');
+    })
+
+})
--- functions.js
+++ functions.js
@@ -1,5 +1,16 @@
+const axios = require("axios");
+
 const functions = {
     add: (num1, num2) => num1+num2,
-    isNULL: () => null
+    isNULL: () => null,
+    checkValue : x => x,
+    createUser : () => {
+        const user = { firstname : 'Vipul' };
+        user['lastname'] = 'Somani';
+        return user;
+    },
+    fetchUser : () => axios.get('https://jsonplaceholder.typicode.com/users/1')
+    .then(res => res.data)
+    .catch( err => 'error')
 };
 module.exports = functions;
